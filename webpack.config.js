7a115cbf693efc631736e8c9bd69e067baf2ee07
ayseren <ayse.eren@windowslive.com>
2021-12-13 15:40:37 +0300

    webpack is created

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const os = require("os");
+const IS_BROWSER = os.platform() === "browser";
+
+let crypto;
+if (IS_BROWSER) {
+  crypto = window.crypto;
+} else {
+  crypto = require("crypto");
+}
+
+module.exports = (env) => {
+  // eslint-disable-next-line no-console
+  // console.log(env.mod);
+
+  return {
+    // mode: env.mod,
+    entry: {
+      scriptWiz: "./src/index.ts",
+    },
+    output: {
+      filename: "index.js",
+      library: "[name]",
+      path: path.resolve(__dirname, "dist"),
+      libraryTarget: "commonjs2",
+    },
+    devtool: "source-map", // devtool: 'cheap-module-source-map',
+    module: {
+      rules: [
+        { test: /\.ts$/, loader: "ts-loader" },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".ts", ".js", ".d.ts"],
+    },
+    externals: {
+      crypto: "crypto",
+    },
+  };
+};
