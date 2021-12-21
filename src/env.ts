9d82a4fc5790f966cc9ac9822d24914382d8670b
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-21 23:47:56 +0300

    env :quotePrecisionCoefficient,tokenPrecisionCoefficient

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,5 @@
 export const poolLbtcLiquidity = 1000000;
 export const poolTokenLiquidity = 50000000000;
 export const lpFeeRate = 500;
+export const quotePrecisionCoefficient = 16;
+export const tokenPrecisionCoefficient = 2000000;
