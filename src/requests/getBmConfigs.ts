b7da6f159f85b283599d64d861c7d8d1e1cd156b
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-21 18:14:39 +0300

    getBmConfigs

---
+++ src/requests/getBmConfigs.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+import { BmConfig } from "@bitmatrix/models";
+
+export const getBmConfigs = () => {
+  axios
+    .get<BmConfig>("https://db.bitmatrix-aggregate.com/config/43a2f4ef8ce286e57ab3e39e6da3741382ba542854a1b28231a7a5b8ba337fcd")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
