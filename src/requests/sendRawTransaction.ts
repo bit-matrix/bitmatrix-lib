389aaf1000851cb7779afe6eebe30fc3ebe0c960
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-21 18:14:29 +0300

    sendRawTransactions

---
+++ src/requests/sendRawTransaction.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+export const sendRawTransaction = (param: string) => {
+  return axios
+    .post(
+      "http://157.230.101.158:9485/rpc",
+      JSON.stringify({
+        jsonrpc: "1.0",
+        id: "curltest",
+        method: "sendrawtransaction",
+        params: [param],
+      }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+    .then((response) => {
+      return response.data.result;
+    })
+    .catch((err) => {
+      return err;
+    });
+};
