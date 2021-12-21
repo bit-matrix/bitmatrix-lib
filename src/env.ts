b6e031ce43632301b4ba8055cddc202b73e36743
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-22 02:31:39 +0300

    env: targetAssetId

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,4 @@ export const poolTokenLiquidity = 50000000000;
 export const lpFeeRate = 500;
 export const quotePrecisionCoefficient = 16;
 export const tokenPrecisionCoefficient = 2000000;
+export const targetAssetId = "43a2f4ef8ce286e57ab3e39e6da3741382ba542854a1b28231a7a5b8ba337fcd";
