51fc3354c1b9e7496a8be70a7b1da32f5cd94ce8
afarukcali <afarukcali@gmail.com>
2021-12-23 01:04:45 +0300

    lp fee rate changed

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const poolLbtcLiquidity = 1000000;
 export const poolTokenLiquidity = 50000000000;
-export const lpFeeRate = 500;
+export const lpFeeRate = 400;
 export const quotePrecisionCoefficient = 16;
 export const tokenPrecisionCoefficient = 2000000;
 export const targetAssetId = "e1ed34f4be34f90408f008c32f932e2b7ebfbfab64ed3e925aab8b635cba5c16";
