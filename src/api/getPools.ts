559d2305a63353a24e8ff61639e67a2e25000f53
afarukcali <afarukcali@gmail.com>
2021-12-23 02:18:42 +0300

    pool api functions params added.

--- src/api/getPools.ts
+++ src/api/getPools.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const getPools = async () => {
+export const getPools = async (poolId: string) => {
   return axios
-    .get("https://db.bitmatrix-aggregate.com/pools/43a2f4ef8ce286e57ab3e39e6da3741382ba542854a1b28231a7a5b8ba337fcd")
+    .get(`https://db.bitmatrix-aggregate.com/pools/${poolId}`)
     .then((response) => {
       return response.data;
     })
