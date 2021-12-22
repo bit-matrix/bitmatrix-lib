7fb1ffeb1808f11df615bd577d4c0e00939612fe
afarukcali <afarukcali@gmail.com>
2021-12-22 23:57:51 +0300

    target pool id changed.

--- src/env.ts
+++ src/env.ts
@@ -3,4 +3,4 @@ export const poolTokenLiquidity = 50000000000;
 export const lpFeeRate = 500;
 export const quotePrecisionCoefficient = 16;
 export const tokenPrecisionCoefficient = 2000000;
-export const targetAssetId = "43a2f4ef8ce286e57ab3e39e6da3741382ba542854a1b28231a7a5b8ba337fcd";
+export const targetAssetId = "e1ed34f4be34f90408f008c32f932e2b7ebfbfab64ed3e925aab8b635cba5c16";
