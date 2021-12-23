840faf02eb0caf916eefc71bb3dcd15381050936
afarukcali <afarukcali@gmail.com>
2021-12-23 16:13:27 +0300

    v.0.0.3

--- dist/env.js
+++ dist/env.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.targetAssetId = exports.tokenPrecisionCoefficient = exports.quotePrecisionCoefficient = exports.lpFeeRate = void 0;
-exports.lpFeeRate = 400;
+exports.lpFeeRate = 500;
 exports.quotePrecisionCoefficient = 16;
 exports.tokenPrecisionCoefficient = 2000000;
 exports.targetAssetId = "e1ed34f4be34f90408f008c32f932e2b7ebfbfab64ed3e925aab8b635cba5c16";
