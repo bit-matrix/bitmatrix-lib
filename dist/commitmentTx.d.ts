14e00e46e7106118481d7b851479ac55f2285280
afarukcali <afarukcali@gmail.com>
2021-12-23 23:08:53 +0300

    dist

--- dist/commitmentTx.d.ts
+++ dist/commitmentTx.d.ts
@@ -1,3 +1,3 @@
 import { BmConfig } from "@bitmatrix/models";
-export declare const quoteToTokenCreateCommitmentTx: (inputAmount: number, txId: string, publicKey: string, calculatedAmountWithSlippage: number, config: BmConfig) => string;
-export declare const tokenToQuoteCreateCommitmentTx: (inputAmount: number, txId: string, publicKey: string, tokenAssetId: string, calculatedAmountWithSlippage: number, config: BmConfig) => string;
+export declare const quoteToTokenCreateCommitmentTx: (inputAmount: number, txId: string, publicKey: string, calculatedAmountWithSlippage: number, config: BmConfig, quoteAssetId: string) => string;
+export declare const tokenToQuoteCreateCommitmentTx: (inputAmount: number, txId: string, publicKey: string, tokenAssetId: string, quoteAssetId: string, calculatedAmountWithSlippage: number, config: BmConfig) => string;
