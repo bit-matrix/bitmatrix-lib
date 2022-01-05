1d923008ba9038f9146562dff099498047acce50
afarukcali <afarukcali@gmail.com>
2022-01-05 22:26:36 +0300

    dist

--- dist/convertion.d.ts
+++ dist/convertion.d.ts
@@ -1,5 +1,5 @@
 import { BmConfig, CALL_METHOD, Pool } from "@bitmatrix/models";
-export declare const convertForCtx: (value: number, slippage: number, pool: Pool, config: BmConfig, callMethod: CALL_METHOD) => {
+export declare const convertForCtx: (value: number, slippage: number, pool: Pool, config: BmConfig, callMethod: CALL_METHOD, constant: number) => {
     amount: number;
     amountWithSlipapge: number;
 };
