f1097edbab911a290369c6595b9cb4e6492fb6b5
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-07 17:37:49 +0300

    index: fundingTxForLiquidity export

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as api from "./api";
 import * as commitmentTx from "./commitmentTx";
 import * as convertion from "./convertion";
 import fundingTx from "./fundingTx";
+import fundingTxForLiquidity from "./fundingTxForLiquidity";
 
-export { api, commitmentTx, convertion, fundingTx };
+export { api, commitmentTx, convertion, fundingTx, fundingTxForLiquidity };
