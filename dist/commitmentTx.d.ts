4390c49268ea7eb928375f500593b24083381304
afarukcali <afarukcali@gmail.com>
2022-01-10 18:05:16 +0300

    dist

--- dist/commitmentTx.d.ts
+++ dist/commitmentTx.d.ts
@@ -2,3 +2,4 @@ import { BmConfig, Pool } from "@bitmatrix/models";
 export declare const quoteToTokenCreateCommitmentTx: (inputAmount: number, txId: string, publicKey: string, calculatedAmountWithSlippage: number, config: BmConfig, pool: Pool) => string;
 export declare const tokenToQuoteCreateCommitmentTx: (inputAmount: number, txId: string, publicKey: string, calculatedAmountWithSlippage: number, config: BmConfig, pool: Pool) => string;
 export declare const liquidityAddCreateCommitmentTx: (quoteAmount: number, tokenAmount: number, txId: string, publicKey: string, config: BmConfig, pool: Pool) => string;
+export declare const liquidityRemoveCreateCommitmentTx: (lpAmount: number, txId: string, publicKey: string, config: BmConfig, pool: Pool) => string;
--- dist/fundingTxForLiquidity.js
+++ dist/fundingTxForLiquidity.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var models_1 = require("@bitmatrix/models");
 var fundingTxForLiquidity = function (quoteAmount, tokenAmount, pool, config, callMethod) {
     var totalFee = config.baseFee.number + config.serviceFee.number + config.commitmentTxFee.number + config.defaultOrderingFee.number;
     var fundingOutput1Value = quoteAmount + totalFee;
@@ -7,7 +8,13 @@ var fundingTxForLiquidity = function (quoteAmount, tokenAmount, pool, config, ca
     var fundingOutput1Address = config.fundingOutputAddress;
     var fundingOutput2Address = config.fundingOutputAddress;
     var fundingOutput1AssetId = pool.quote.asset;
-    var fundingOutput2AssetId = pool.token.asset;
+    var fundingOutput2AssetId = "";
+    if (callMethod === models_1.CALL_METHOD.ADD_LIQUIDITY) {
+        fundingOutput2AssetId = pool.token.asset;
+    }
+    else if (callMethod === models_1.CALL_METHOD.REMOVE_LIQUIDITY) {
+        fundingOutput2AssetId = pool.lp.asset;
+    }
     return {
         fundingOutput1Value: fundingOutput1Value,
         fundingOutput2Value: fundingOutput2Value,
