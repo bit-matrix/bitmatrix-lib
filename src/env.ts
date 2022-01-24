ec55a10b3cc24624fa34404e3d9f77f4a4e3cfc0
afarukcali <afarukcali@gmail.com>
2022-01-24 16:25:54 +0300

    Db url updated.

--- src/env.ts
+++ src/env.ts
@@ -3,5 +3,5 @@ export const quotePrecisionCoefficient = 16;
 export const tokenPrecisionCoefficient = 2000000;
 export const rpcUrl = "https://rpc.bitmatrix-aggregate.com/rpc";
 // export const bmUrl = "https://db.bitmatrix-aggregate.com";
-export const bmUrl = "https://rocksdb.bitmatrix-aggregate.com/";
+export const bmUrl = "https://rocksdb.bitmatrix-aggregate.com";
 export const bmApiUrl = "https://api.bitmatrix-aggregate.com";
