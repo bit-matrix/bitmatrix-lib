76cbdbb4639bbe0d2cdb205eed61f91352d1036c
afarukcali <afarukcali@gmail.com>
2022-01-24 16:11:04 +0300

    dist

--- dist/env.d.ts
+++ dist/env.d.ts
@@ -2,5 +2,5 @@ export declare const lpFeeRate = 500;
 export declare const quotePrecisionCoefficient = 16;
 export declare const tokenPrecisionCoefficient = 2000000;
 export declare const rpcUrl = "https://rpc.bitmatrix-aggregate.com/rpc";
-export declare const bmUrl = "https://db.bitmatrix-aggregate.com";
+export declare const bmUrl = "https://rocksdb.bitmatrix-aggregate.com/";
 export declare const bmApiUrl = "https://api.bitmatrix-aggregate.com";
