6058bcd5395b74eb0964f5e026903b3eacb5c953
afarukcali <afarukcali@gmail.com>
2022-01-25 17:26:18 +0300

    New pool api url updated.

--- src/env.ts
+++ src/env.ts
@@ -4,4 +4,4 @@ export const tokenPrecisionCoefficient = 2000000;
 export const rpcUrl = "https://rpc.bitmatrix-aggregate.com/rpc";
 // export const bmUrl = "https://db.bitmatrix-aggregate.com";
 export const bmUrl = "https://rocksdb.bitmatrix-aggregate.com";
-export const bmApiUrl = "https://api.bitmatrix-aggregate.com";
+export const bmApiUrl = "https://rocksapi.bitmatrix-aggregate.com";
