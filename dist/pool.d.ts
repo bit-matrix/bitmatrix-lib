b1040027f692cb9b1bb6edf960a6a89372256c1d
afarukcali <afarukcali@gmail.com>
2022-04-25 21:21:22 +0300

    dist

--- dist/pool.d.ts
+++ dist/pool.d.ts
@@ -4,3 +4,9 @@ export declare const createCovenants: (leafCount: number, lookupLeafIndex: numbe
     controlBlock: string;
     taprootResult: import("@script-wiz/lib-core/taproot/model").Taproot;
 };
+export declare const bodyCalculaterNV2: (n: number, flagAssetId: string, pair1Coefficient?: number) => string;
+export declare const createCovenantsV2: (leafCount: number, lookupLeafIndex: number, flagAssetId: string, pair1Coefficient?: number | undefined) => {
+    mainCovenantScript: string[];
+    controlBlock: string;
+    taprootResult: import("@script-wiz/lib-core/taproot/model").Taproot;
+};
