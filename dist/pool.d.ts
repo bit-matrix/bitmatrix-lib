bd588a88f486334ca69c4bcf0ad63470f8297fd6
afarukcali <afarukcali@gmail.com>
2022-04-27 13:59:24 +0300

    dist

--- dist/pool.d.ts
+++ dist/pool.d.ts
@@ -1,11 +1,5 @@
-export declare const bodyCalculaterN: (n: number, flagAssetId: string) => string;
-export declare const createCovenants: (leafCount: number, lookupLeafIndex: number, flagAssetId: string) => {
-    mainCovenantScript: string[];
-    controlBlock: string;
-    taprootResult: import("@script-wiz/lib-core/taproot/model").Taproot;
-};
-export declare const bodyCalculaterNV2: (n: number, flagAssetId: string, pair1Coefficient?: number) => string;
-export declare const createCovenantsV2: (leafCount: number, lookupLeafIndex: number, flagAssetId: string, pair1Coefficient?: number | undefined) => {
+export declare const bodyCalculaterN: (n: number, flagAssetId: string, pair1Coefficient?: number) => string;
+export declare const createCovenants: (leafCount: number, lookupLeafIndex: number, flagAssetId: string, pair1Coefficient?: number | undefined) => {
     mainCovenantScript: string[];
     controlBlock: string;
     taprootResult: import("@script-wiz/lib-core/taproot/model").Taproot;
