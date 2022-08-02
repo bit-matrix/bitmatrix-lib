fde227b16b2c015ef735cc5e0cacac3b585eb6d8
afarukcali <afarukcali@gmail.com>
2022-08-02 15:21:48 +0300

    unused model removed.

--- src/model/FundingOutput.ts
+++
@@ -1,8 +0,0 @@
-export type FundingOutput = {
-  fundingOutput1Value: number;
-  fundingOutput2Value: number;
-  fundingOutput1Address: string;
-  fundingOutput2Address: string;
-  fundingOutput1AssetId: string;
-  fundingOutput2AssetId: string;
-};
