a4d0598fb7016fd951eed7e4f572a6050395d538
afarukcali <afarukcali@gmail.com>
2022-09-01 18:56:01 +0300

    dist

--- dist/validatePoolTx.d.ts
+++ dist/validatePoolTx.d.ts
@@ -1,2 +1,5 @@
-import { CTXFinderResult, Pool, PTXFinderResult } from "@bitmatrix/models";
-export declare const validatePoolTx: (value: CTXFinderResult, poolData: Pool) => Promise<PTXFinderResult>;
+import { CALL_METHOD, Pool } from "@bitmatrix/models";
+export declare const validatePoolTx: (value: number, slippageTolerance: number, poolData: Pool, methodCall: CALL_METHOD, additionalValue?: number) => {
+    amount: number;
+    amountWithSlipapge: number;
+};
