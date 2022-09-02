d62a9a2d780ac200e76a2d1c5e213b3ea56c872e
afarukcali <afarukcali@gmail.com>
2022-09-02 20:45:44 +0300

    lpFeeTier revert.

--- src/poolDeployment.ts
+++ src/poolDeployment.ts
@@ -44,7 +44,7 @@ export const poolDeploy = (
     leafCount = 63;
   }
 
-  const lpFeeTier = convertion.convert64(WizData.fromNumber(lpFeeTierIndex));
+  const lpFeeTier = lpFeeTierIndex === 0 ? WizData.fromHex("00") : WizData.fromNumber(lpFeeTierIndex);
 
   const mainCovenantScriptPubkey = createCovenants(leafCount, 0, newFlagAssetId, pair1Coefficient, lpFeeTierIndex).taprootResult.scriptPubkey.hex;
 
