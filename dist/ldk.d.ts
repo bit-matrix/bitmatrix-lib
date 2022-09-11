63f68437817eaf3d7d671b7302e84e06aac8e139
afarukcali <afarukcali@gmail.com>
2022-09-11 19:06:20 +0300

    dist

--- dist/ldk.d.ts
+++ dist/ldk.d.ts
@@ -1,3 +1,3 @@
 import { RecipientInterface } from "ldk";
 import { Wallet } from "./wallet";
-export declare const signTx: (marina: Wallet, callData: string, recipients: RecipientInterface[]) => Promise<string>;
+export declare const signTx: (marina: Wallet, callData: string, recipients: RecipientInterface[], isTestnet?: boolean) => Promise<string>;
--- dist/ldk.js
+++ dist/ldk.js
@@ -65,53 +65,56 @@ var liquidjs_lib_1 = require("liquidjs-lib");
 var ecc = __importStar(require("tiny-secp256k1"));
 var utils_1 = require("./utils/utils");
 var helper_1 = require("./utils/helper");
-var signTx = function (marina, callData, recipients) { return __awaiter(void 0, void 0, void 0, function () {
-    var coins, pset, tx, makeGetter, assets, uniqueAssets, changeAddressGetter, unsignedTx, ptx, inputBlindingMap, outputBlindingMap, signedTx, finalTx, txFinal;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0: return [4 /*yield*/, marina.getCoins()];
-            case 1:
-                coins = _a.sent();
-                pset = new liquidjs_lib_1.Psbt({ network: liquidjs_lib_1.networks.testnet });
-                tx = pset.toBase64();
-                makeGetter = makeAssetChangeGetter(marina);
-                assets = recipients.map(function (r) { return r.asset; });
-                uniqueAssets = (0, helper_1.uniqueArray)(assets);
-                return [4 /*yield*/, makeGetter(uniqueAssets)];
-            case 2:
-                changeAddressGetter = _a.sent();
-                unsignedTx = (0, ldk_1.craftMultipleRecipientsPset)({
-                    psetBase64: tx,
-                    unspents: coins,
-                    recipients: recipients,
-                    coinSelector: (0, ldk_1.greedyCoinSelector)(),
-                    changeAddressByAsset: changeAddressGetter,
-                    addFee: true,
-                });
-                ptx = liquidjs_lib_1.Psbt.fromBase64(unsignedTx);
-                ptx.addOutput({
-                    script: liquidjs_lib_1.script.compile([liquidjs_lib_1.script.OPS.OP_RETURN, Buffer.from(callData, "hex")]),
-                    value: liquidjs_lib_1.confidential.satoshiToConfidentialValue(0),
-                    asset: liquidjs_lib_1.AssetHash.fromHex(liquidjs_lib_1.networks.testnet.assetHash, false).bytes,
-                    nonce: Buffer.alloc(0),
-                });
-                inputBlindingMap = (0, utils_1.inputBlindingDataMap)(unsignedTx, coins);
-                outputBlindingMap = (0, utils_1.outPubKeysMap)(unsignedTx, [changeAddressGetter(liquidjs_lib_1.networks.testnet.assetHash), recipients[0].address]);
-                return [4 /*yield*/, ptx.blindOutputsByIndex(liquidjs_lib_1.Psbt.ECCKeysGenerator(ecc), inputBlindingMap, outputBlindingMap)];
-            case 3:
-                _a.sent();
-                return [4 /*yield*/, marina.signTransaction(ptx.toBase64())];
-            case 4:
-                signedTx = _a.sent();
-                finalTx = liquidjs_lib_1.Psbt.fromBase64(signedTx);
-                finalTx.finalizeAllInputs();
-                return [4 /*yield*/, marina.broadcastTransaction(finalTx.extractTransaction().toHex())];
-            case 5:
-                txFinal = _a.sent();
-                return [2 /*return*/, txFinal.txid];
-        }
+var signTx = function (marina, callData, recipients, isTestnet) {
+    if (isTestnet === void 0) { isTestnet = false; }
+    return __awaiter(void 0, void 0, void 0, function () {
+        var coins, pset, tx, makeGetter, assets, uniqueAssets, changeAddressGetter, unsignedTx, ptx, inputBlindingMap, outputBlindingMap, signedTx, finalTx, txFinal;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0: return [4 /*yield*/, marina.getCoins()];
+                case 1:
+                    coins = _a.sent();
+                    pset = new liquidjs_lib_1.Psbt({ network: isTestnet ? liquidjs_lib_1.networks.testnet : liquidjs_lib_1.networks.liquid });
+                    tx = pset.toBase64();
+                    makeGetter = makeAssetChangeGetter(marina);
+                    assets = recipients.map(function (r) { return r.asset; });
+                    uniqueAssets = (0, helper_1.uniqueArray)(assets);
+                    return [4 /*yield*/, makeGetter(uniqueAssets)];
+                case 2:
+                    changeAddressGetter = _a.sent();
+                    unsignedTx = (0, ldk_1.craftMultipleRecipientsPset)({
+                        psetBase64: tx,
+                        unspents: coins,
+                        recipients: recipients,
+                        coinSelector: (0, ldk_1.greedyCoinSelector)(),
+                        changeAddressByAsset: changeAddressGetter,
+                        addFee: true,
+                    });
+                    ptx = liquidjs_lib_1.Psbt.fromBase64(unsignedTx);
+                    ptx.addOutput({
+                        script: liquidjs_lib_1.script.compile([liquidjs_lib_1.script.OPS.OP_RETURN, Buffer.from(callData, "hex")]),
+                        value: liquidjs_lib_1.confidential.satoshiToConfidentialValue(0),
+                        asset: liquidjs_lib_1.AssetHash.fromHex(liquidjs_lib_1.networks.testnet.assetHash, false).bytes,
+                        nonce: Buffer.alloc(0),
+                    });
+                    inputBlindingMap = (0, utils_1.inputBlindingDataMap)(unsignedTx, coins);
+                    outputBlindingMap = (0, utils_1.outPubKeysMap)(unsignedTx, [changeAddressGetter(liquidjs_lib_1.networks.testnet.assetHash), recipients[0].address]);
+                    return [4 /*yield*/, ptx.blindOutputsByIndex(liquidjs_lib_1.Psbt.ECCKeysGenerator(ecc), inputBlindingMap, outputBlindingMap)];
+                case 3:
+                    _a.sent();
+                    return [4 /*yield*/, marina.signTransaction(ptx.toBase64())];
+                case 4:
+                    signedTx = _a.sent();
+                    finalTx = liquidjs_lib_1.Psbt.fromBase64(signedTx);
+                    finalTx.finalizeAllInputs();
+                    return [4 /*yield*/, marina.broadcastTransaction(finalTx.extractTransaction().toHex())];
+                case 5:
+                    txFinal = _a.sent();
+                    return [2 /*return*/, txFinal.txid];
+            }
+        });
     });
-}); };
+};
 exports.signTx = signTx;
 var makeAssetChangeGetter = function (marina) {
     return function (assets) { return __awaiter(void 0, void 0, void 0, function () {
